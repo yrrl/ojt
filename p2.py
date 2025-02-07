9ae18edeb11f1e5ed99e79cb7cf29e3f3e1da7a0
yrrel <fulejohnlerry1112@gmail.com>
2025-02-07 16:08:45 +0800

    main

---
+++ p2.py
@@ -0,0 +1,8 @@
+def main():
+    list_of_users = ("BookwormBabe", "GamerGirl99", "WanderlustJoe", "CreativeMind", "FoodieFlora", "FitnessFan", "PositivePanda", "CuriousCharlie")
+    
+    for i in enumerate(list_of_users):
+        print(i[0] + 1,i[-1:])
+
+if __name__ == "__main__":
+    main()
