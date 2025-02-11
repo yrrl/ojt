8a4cc744682aa947639f7bbac34b5ce0ed436043
yrrel <fulejohnlerry1112@gmail.com>
2025-02-11 15:33:36 +0800

    main

---
+++ crude.py
@@ -0,0 +1,65 @@
+# CRUD operations
+
+# Create
+# Read
+# Update
+# Delete
+import psycopg2
+
+
+def create_person(conn, firstname, lastname, age, course):
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO studlist (firstname, lastname, age, course) VALUES (%s, %s, %s, %s)", (firstname, lastname, age, course))
+    conn.commit()
+    cursor.close()
+
+def read_all_persons(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM studlist")
+    stud = cursor.fetchall()
+    cursor.close()
+    return stud
+
+def read_person(conn, stud_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM studlist where id=%s", str(stud_id))
+    stud = cursor.fetchone()
+    cursor.close()
+    return stud
+
+def update_name(conn, stud_id, name):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE studlist set firstname=%s WHERE id=%s", (name, stud_id))
+    print("name updated!!!!!!! ")
+    cursor.execute("SELECT * FROM studlist where id=%s", str(stud_id))
+    stud = cursor.fetchone()
+    cursor.close()
+
+
+def delete_person(conn, person_id):
+    pass
+
+
+if __name__ == "__main__":
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="yrrel",
+        password="11lordgrim"
+    )
+    temp = read_person(conn,2)
+    print(temp)
+
+    update = update_name(conn,2,'maria')
+    #update_name(conn,stud_id,name)
+
+
+   # create_person(conn, "John", "Doe", 30, "BSIT")
+   # stud = read_all_persons(conn)
+   # print( "UID" ,"||", "FirstName" ,"||", "LastName" ,"||", "Course")
+   # for persons in stud:
+        
+    #   print( persons[0] ,"||", persons[1] ,"||", persons[2],"||",persons[3])
+
+
+  #  print(persons[0][1])
