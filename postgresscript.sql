8f04085ee766734a97b489b077e9ac7a943c5b39
yrrel <fulejohnlerry1112@gmail.com>
2025-02-12 13:30:22 +0800

    main

---
+++ postgresscript.sql
@@ -0,0 +1,21 @@
+create schema student_list;
+drop table studlist;
+create table studlist (
+	id SERIAL primary key,
+	firstname VARCHAR(256) not null,
+	lastname VARCHAR(256) not null,
+	age INT,
+	course VARCHAR(10)
+	
+)
+
+
+
+insert into studlist(firstname,lastname,age,course)
+values ('john','lerry',22,'BSINFO'),('juan','juuu',29,'BSINFO');
+
+select * from studlist;
+
+UPDATE studlist set firstname='juasn' WHERE id=2;
+
+
